492ee21da8cc1c868c22c7542aae394972453524
xnlou <xnlouey@gmail.com>
2019-02-17 20:45:23 -0600

    a <p> with red text

--- dom_java.js
+++ dom_java.js
@@ -6,3 +6,19 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// Create new paragraph with red text
+
+    // create a new paragraph for variable 'p'
+    const par = document.createElement('p');
+    // Styles text in red
+    par.style.color ='red';
+
+    // Create class for 1st paragraph
+    par.classList.add('paragraph_1');
+
+    // add text to display
+    par.textContent = "Hey I'm red!";
+
+    // appends the paragraph to the container object
+    container.appendChild(par);
